8bf6bbd4b7849c07f63bcaccc2fe30d056b55cdd
Jose Giraldo <jose091@gmail.com>
2023-05-05 08:38:33 +0200

    main format

--- voicefixer/__main__.py
+++ voicefixer/__main__.py
@@ -91,7 +91,6 @@ if __name__ == "__main__":
         default=False,
         action="store_true",
     )
-
     args = parser.parse_args()
 
     if torch.cuda.is_available() and not args.disable_cuda:
